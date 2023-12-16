ee72674a33601b7760bd66b7ab64a35bf1d135b8
Ahmad Alrai <ahmadhalraie@gmail.com>
2023-12-16 11:05:57 +0300

    242 JS

---
+++ 242. Valid Anagram/Solution.js
@@ -0,0 +1,19 @@
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  var freq = new Array(26).fill(0);
+  for (var i = 0; i < s.length; i++) {
+    freq[s.charCodeAt(i) - "a".charCodeAt(0)]++;
+    freq[t.charCodeAt(i) - "a".charCodeAt(0)]--;
+  }
+
+  for (var i = 0; i < freq.length; i++) {
+    if (freq[i] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
