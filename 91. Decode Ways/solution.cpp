7463eb8f48219134cb22ed00cdc9dc32ce5880bc
Ahmad Alrai <ahmadhalraie@gmail.com>
2023-12-25 09:38:21 +0300

    91 C++

---
+++ 91. Decode Ways/solution.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+
+    // Approach 1: Recursion O(2^n). A char may be decoded alone or by pairing with the next char.
+
+    int numDecodings1(string s) {
+        return s.empty() ? 0: numDecodings1(0,s);    
+    }
+    int numDecodings1(int p, string& s) {
+        int n = s.size();
+        if(p == n) return 1;
+        if(s[p] == '0') return 0; // sub string starting with 0 is not a valid encoding
+        int res = numDecodings1(p+1,s);
+        if( p < n-1 && (s[p]=='1'|| (s[p]=='2'&& s[p+1]<'7'))) res += numDecodings1(p+2,s);
+        return res;
+    }
+
+    // Results: Time Limit Exceeded - 23 / 269 testcases passed
+
+    // Approach 2: Memoization O(n)
+
+    int numDecodings2(string s) {
+        int n = s.size();
+        vector<int> mem(n+1,-1);
+        mem[n]=1;
+        return s.empty()? 0 : num(0,s,mem);   
+    }
+    int num(int i, string &s, vector<int> &mem) {
+        if(mem[i]>-1) return mem[i];
+        if(s[i]=='0') return mem[i] = 0;
+        int res = num(i+1,s,mem);
+        if(i<s.size()-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) res+=num(i+2,s,mem);
+        return mem[i] = res;
+    }
+
+    // Approach 3: Dynamic Programming - O(n) time and space, this can be converted from #2 with copy and paste.
+
+        int numDecodings3(string s) {
+        int n = s.size();
+        vector<int> dp(n+1);
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--) {
+            if(s[i]=='0') dp[i]=0;
+            else {
+                dp[i] = dp[i+1];
+                if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) dp[i]+=dp[i+2];
+            }
+        }
+        return s.empty()? 0 : dp[0];   
+    }
+
+    // Approach 4: DP - Constant space
+
+        int numDecodings(string s) {
+        int p = 1, pp, n = s.size();
+        for(int i=n-1;i>=0;i--) {
+            int cur = s[i]=='0' ? 0 : p;
+            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
+            pp = p;
+            p = cur;
+        }
+        return s.empty()? 0 : p;   
+    }
+
+
+};
