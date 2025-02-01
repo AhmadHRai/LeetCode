5ae99f56d324d8801514f5071000bda2e9e84931
AhmadHRai <ahmadhalraie@gmail.com>
2025-02-01 21:09:40 +0300

    3151 C++ %

---
+++ 3001+/3151. Special Array I/Modulus.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    public:
+        bool isArraySpecial(vector<int>& nums) {
+            // Iterate through indexes 0 to n - 1
+            for (int i = 0; i < nums.size() - 1; i++) {
+                // Compare the parities of the current and next number
+                if (nums[i] % 2 == nums[i + 1] % 2) {
+                    // If the two adjacent numbers have the same parity, return
+                    // false
+                    return false;
+                }
+            }
+    
+            // Return true if no pair of adjacent numbers with the same parity are
+            // found
+            return true;
+        }
+    };
+
+/* 
+Algorithm
+
+    Iterate through nums from index 0 to n - 1, where n is the length of nums:
+        For each index i, compare the parities of numbers nums[i] and nums[i + 1].
+            If nums[i] % 2 equals nums[i + 1] % 2, there are two adjacent numbers with the same parity, so it returns false.
+    If the loop completes without finding two adjacent numbers with the same parity, return true, indicating the array is special.
+
+Complexity Analysis
+    Let n be the size of the nums array.
+
+    Time complexity: O(n)
+
+        In the best case, we find a pair of adjacent numbers with the same parity on the first iteration of the loop, which would take only O(1) time.
+        In the worst case, the program has to iterate n−1 times to return a result if no adjacent numbers with the same parity are found, taking O(n−1) time.
+        As a result, this leads to an overall time complexity of O(n−1), which can be simplified to O(n).
+
+    Space complexity: O(1)
+        The space required does not depend on the size of the input array or any data structures that require additional space, so only constant O(1) space is used.
+
+*/
