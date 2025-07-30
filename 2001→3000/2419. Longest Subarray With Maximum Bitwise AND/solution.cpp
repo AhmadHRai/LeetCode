e43991e05d8416c4e4304141f4ed308120592357
AhmadHRai <ahmadhalraie@gmail.com>
2025-07-30 13:05:50 +0300

    2419 C++ (unchanged)

--- 2001→3000/2419. Longest Subarray With Maximum Bitwise AND/solution.cpp
+++ 2001→3000/2419. Longest Subarray With Maximum Bitwise AND/solution.cpp
@@ -36,17 +36,17 @@ public:
 
 /*
 Approach:
-1. Initialize variables to keep track of the maximum value encountered, the length of the longest subarray with the maximum value, and the current streak of the maximum value.
-2. Iterate through each element in the input array:
-   - If the current element is greater than the current maximum value, update the maximum value and reset the lengths of the longest subarray and current streak.
-   - If the current element equals the maximum value, increment the length of the current streak.
-   - If the current element is less than the maximum value, reset the current streak to 0.
-3. Continuously update the length of the longest subarray with the maximum value based on the length of the current streak.
-4. Return the length of the longest subarray with the maximum value after processing all elements.
+    1. Initialize variables to keep track of the maximum value encountered, the length of the longest subarray with the maximum value, and the current streak of the maximum value.
+    2. Iterate through each element in the input array:
+    - If the current element is greater than the current maximum value, update the maximum value and reset the lengths of the longest subarray and current streak.
+    - If the current element equals the maximum value, increment the length of the current streak.
+    - If the current element is less than the maximum value, reset the current streak to 0.
+    3. Continuously update the length of the longest subarray with the maximum value based on the length of the current streak.
+    4. Return the length of the longest subarray with the maximum value after processing all elements.
 
-Time Complexity:
-- O(n), where `n` is the number of elements in the array. This is because we make a single pass through the array, performing constant-time operations for each element.
+    Time Complexity:
+    - O(n), where `n` is the number of elements in the array. This is because we make a single pass through the array, performing constant-time operations for each element.
 
-Space Complexity:
-- O(1), as we are using only a fixed amount of extra space for the variables (`maxVal`, `ans`, `currentStreak`) and not using any additional data structures that grow with the input size.
+    Space Complexity:
+    - O(1), as we are using only a fixed amount of extra space for the variables (`maxVal`, `ans`, `currentStreak`) and not using any additional data structures that grow with the input size.
 */
