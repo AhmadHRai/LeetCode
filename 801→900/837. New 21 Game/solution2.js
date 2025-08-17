47b4a155053275571efb981d15ff6be464b13950
AhmadHRai <ahmadhalraie@gmail.com>
2025-08-17 11:40:17 +0300

    837 js 2

---
+++ 801→900/837. New 21 Game/solution2.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} maxPts
+ * @return {number}
+ */
+var new21Game = function (n, k, maxPts) {
+  if (k === 0 || n >= k + maxPts) {
+    return 1;
+  }
+
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
+  let sum = 1;
+
+  for (let i = 1; i <= n; i++) {
+    dp[i] = sum / maxPts;
+
+    if (i < k) {
+      sum += dp[i];
+    }
+
+    if (i - maxPts >= 0) {
+      sum -= dp[i - maxPts];
+    }
+  }
+
+  let result = 0;
+  for (let j = k; j <= n; j++) {
+    result += dp[j];
+  }
+
+  return result;
+};
